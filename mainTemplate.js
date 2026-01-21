c81ba6a816f440cb16b93f638b91014c897b536a
ripmodex <botond.ozvegy@gmail.com>
2026-01-21 22:40:12 +0100

    0.4 started the imagemap, and finally i found out the way to make it how i want, but for now we rest a bit

--- mainTemplate.js
+++ mainTemplate.js
@@ -13,3 +13,84 @@ document.addEventListener("DOMContentLoaded", function() {
         window.open("signup.html", "_blank");
     })
 })
+
+function openMap(){
+    document.getElementById("mapOverlay").style.display="flex"
+    //setTimeout(makeMapResponsive, 50);
+}
+
+function closeMap(){
+    document.getElementById("mapOverlay").style.display="none";
+}
+
+window.onclick=function(event){
+    let overlay=document.getElementById("mapOverlay");
+    if(event.target==overlay){
+        overlay.style.display="none";
+    }
+}
+
+const mapImage=document.getElementById("mapDisplay");
+const originalSrc="Kepek/Map/main.jpg";
+
+function changeImage(newSrc){
+    mapImage.src=newSrc;
+}
+
+function resetImage(){
+    mapImage.src=originalSrc;
+}
+
+document.getElementById('mapDisplay').addEventListener('click', function(e) {
+    const rect = this.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    console.log("Current Pixel Coords: " + Math.round(x) + ", " + Math.round(y));
+});
+//just for debugging, but it clearly did not worked the way i wanted, so here it is
+
+/*function makeMapResponsive(){
+    const img=document.getElementById("mapImage");
+    const areas=map.getElementByTagName('area');
+
+    const ratioX=img.clientWidth/img.naturalWidth;
+    const ratioY=img.clientHeight/img.naturalHeight;
+
+    area.forEach(area=>{
+        if(!area.dataset.original){
+            area.dataset.original=area.getAttribute('coords');
+        }
+        const coords=area.dataset.original.split(',');
+        const scaledCoords=coords.map((c,i)=>{
+            return i%2===0 ? Math.round(c*ratioX) : Math.round(c*ratioY);
+        });
+        area.setAttribute('coords',scaledCoords.join(','));
+    });
+}*/
+
+//window.addEventListener("resize", makeMapResponsive);
+
+const preloadImages=[
+    "Kepek/Map/main.jpg",
+    "Kepek/Map/forgottenCrossroads.jpg",
+    "Kepek/Map/ancientBasin.jpg",
+    "Kepek/Map/cityofTears.jpg",
+    "Kepek/Map/crystalPeak.jpg",
+    "Kepek/Map/deepnest.jpg",
+    "Kepek/Map/dirtmouth.jpg",
+    "Kepek/Map/fogCanyon.jpg",
+    "Kepek/Map/forgottenCrossroads.jpg",
+    "Kepek/Map/fungalWastes.jpg",
+    "Kepek/Map/greenpath.jpg",
+    "Kepek/Map/howlingCliffs.jpg",
+    "Kepek/Map/kingdomsEdge.jpg",
+    "Kepek/Map/queensGarden.jpg",
+    "Kepek/Map/restingGrounds.jpg",
+    "Kepek/Map/royalWaterways.jpg",
+    "Kepek/Map/theHive.jpg",
+]
+
+preloadImages.forEach(src=>{
+    const img=new Image();
+    img.src=src;
+})
